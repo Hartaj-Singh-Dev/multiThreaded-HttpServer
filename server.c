380aba88eb2d51276cda905dab2b1e6f911c103c
Hartaj Singh Sidhu <hartajsinghsidhu2005@gmail.com>
2026-06-07 20:51:29 +0530

    Multithreading added , but not Pool wise , it will grow till number of users grow

--- server.c
+++ server.c
@@ -1,4 +1,7 @@
+#include <netinet/in.h>
+#include<arpa/inet.h>
 #include<stdio.h>
+#include<pthread.h>
 #include<unistd.h>
 #include<sys/socket.h>
 #include<sys/types.h>
@@ -8,13 +11,60 @@
 
 #define LISTEN_BACKLOG 50
 
+void handle_client(int client_fd){
+        sleep(10);
+        int MAX_LEN = 1000;
+		char http_request[MAX_LEN];
+		memset(http_request, 0, MAX_LEN);
+		int num_bytes = recv(client_fd ,http_request , MAX_LEN , 0);
+		if(num_bytes < 0){
+		perror("recv Error");
+		};
+		
+		if(strncmp(http_request , "GET" , 3) == 0){
+		printf("Received HTTP GET request :\n");
+		    // Now we can responsed to HTTP GET request here ;
+			// REad index.html here and then send it to client --> 
+				char*header = "HTTP/1.1 200 OK\r\n"
+				                "Content-Type: text/html\r\n"
+								"Connection: close\r\n"
+								"\r\n";
+				send(client_fd , header , strlen(header) , 0);
+			    FILE *index_fd = fopen("index.html" , "r");
+				if(index_fd == NULL){
+				printf("Fopen error");
+				close(client_fd);
+				return;
+				}
+				char c;
+				while((c = getc(index_fd)) != EOF){
+				int dataSent = send(client_fd, &c ,1, 0);	
+				}
+				
+		}
+		
+	close(client_fd); 
+}
+
+void* thread_function(void* arg){
+    int client_fd = *(int*)arg;
+    free(arg);
+
+    printf(
+        "Thread %lu | fd=%d \n",
+        (unsigned long)pthread_self(),
+        client_fd); 
+    handle_client(client_fd);
+    return NULL;
+};
+
+
 int main(){
 	int socfd = socket(AF_INET ,SOCK_STREAM  ,0 );
 	if(socfd < 0){ perror("Socket error"); 
-	// exit(-1);
+	exit(-1);
 	}
-
-
+	
 	struct addrinfo  hint , *res;	
 	memset(&hint , 0 , sizeof(hint));
 	hint.ai_family = AF_INET;
@@ -32,40 +82,39 @@ int main(){
 	}
 	//Listen to incoming conncetions !!
 	 if(listen(socfd , LISTEN_BACKLOG) < 0){
-		perror("Listen() error");
+		printf("Listen() error");
 		exit(-1);
-		};
+	};
 		
 	struct sockaddr_storage their_addr;
 	socklen_t addr_size = sizeof(their_addr);
-int client_fd = accept(socfd, (struct  sockaddr *) &their_addr, &addr_size);
-	if(client_fd< 0){
-	perror("accept error");
-	exit(-1);
-	  };
+
 	
-	    int MAX_LEN = 1000;
-		char http_request[MAX_LEN];
-		memset(http_request, 0, MAX_LEN);
-		int num_bytes = recv(client_fd ,http_request , MAX_LEN , 0);
-		if(num_bytes < 0){
-		perror("recv Error");
-		}
-		if(strncmp(http_request , "GET" , 3) == 0){
-		printf("Received HTTP GET request :\n");
-		    // Now we can responsed to HTTP GET request here ;
-			// REad index.html here and then send it to client --> 
-				
-			    FILE *index_fd = fopen("index.html" , "r");
-				char c;
-				while((c = getc(index_fd)) != EOF){
-				int dataSent = send(client_fd, &c ,1, 0);	
-				}
-				
-		}
-		
+
+	
+	while(1){
+	    int client_fd = accept(socfd, (struct  sockaddr *) &their_addr, &addr_size);
+		if(client_fd< 0){
+		perror("accept error");
+        continue;
+		  };
+	
+		char ipstr[INET_ADDRSTRLEN];
+		struct sockaddr_in *addr = (struct sockaddr_in *)&their_addr;
+	  inet_ntop(AF_INET , &addr->sin_addr , ipstr , sizeof(ipstr));
+		printf("Accepted Connection from %s fd=%d\n" , 
+		ipstr , client_fd);	
+		pthread_t thread1;
+		int *fd_ptr = malloc(sizeof(int));
+		*fd_ptr = client_fd;
+		if(pthread_create(&thread1, NULL, thread_function, fd_ptr) != 0){
+		perror("pthread_create Error");
+		close(client_fd);
+		free(fd_ptr);
+		continue;
+		};
+		pthread_detach(thread1);
+	}
 	close(socfd);
-	close(client_fd);
-		
 	return 0;
 }
